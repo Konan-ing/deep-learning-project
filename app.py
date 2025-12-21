acecde258565e515c5e86d17399f4bedaf65960f
Konan Kramo <fabriceemmanuel2@gmail.com>
2025-12-21 20:00:08 +0100

    modif de app.py

--- app.py
+++ app.py
@@ -22,4 +22,6 @@ def predict():
     return jsonify({'resultat': float(prediction[0][0])})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7860)
+    # Récupère le port envoyé par l'hébergeur, sinon utilise 7860
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host='0.0.0.0', port=port)
